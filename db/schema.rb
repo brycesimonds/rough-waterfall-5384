df73dab492d0577ef1bae9662f38ba40da1cc389
Bryce Simonds <bsimonds14@gmail.com>
2022-07-31 13:10:54 -0600

    Reset the schema with some drop, create, migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_25_154645) do
+ActiveRecord::Schema.define(version: 2022_07_31_190832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2022_07_25_154645) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["supermarket_id"], name: "index_customers_on_supermarket_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "supermarkets", force: :cascade do |t|
