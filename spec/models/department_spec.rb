166d3266cbef1d9dfcd5f8467f76409432fc3ebf
Bryce Simonds <bsimonds14@gmail.com>
2022-07-31 13:07:36 -0600

    Deleted more incorrect spec files

--- spec/models/department_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Department, type: :model do
-  describe 'relationships' do
-    it { should belong_to :XXX }
-  end
-end
--- spec/models/employee_ticket_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe EmployeeTicket, type: :model do
-  describe 'relationships' do
-    it { should belong_to :supermarket }
-  end
-end
