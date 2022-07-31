dfe00e28c3563da1bec3285561d37b347b9639d9
Bryce Simonds <bsimonds14@gmail.com>
2022-07-31 13:36:11 -0600

    Test: has a list of the customers items

---
+++ spec/features/customer/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'the customer show' do
+  it "has a list of the customers items" do
+    supermarket_1 = Supermarket.create!(name: "Whole Foods", location: "Denver")
+
+    customer_1 = Customer.create!(name: "Hellfire", supermarket_id: supermarket_1.id)
+
+    item_1 = Item.create!(name: "PB", price: 2)
+    item_2 = Item.create!(name: "Jelly", price: 4)
+    item_3 = Item.create!(name: "Bread", price: 6)
+    item_4 = Item.create!(name: "Hot Sauce", price: 8)
+
+    customer_item_1 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_1.id)
+    customer_item_2 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_2.id)
+    customer_item_3 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_3.id)
+
+    visit "/customers/#{customer_1.id}"
+
+    expect(page).to have_content("PB")
+    expect(page).to have_content("Jelly")
+    expect(page).to have_content("Bread")
+    expect(page).to_not have_content("Hot Sauce")
+  end
+end
