f7a8461a4ab4d4f25cd85790c5c0c36c10b9f6c2
Bryce Simonds <bsimonds14@gmail.com>
2022-07-31 14:19:14 -0600

    Test: the link takes you to the supermarkets item index page

--- spec/features/supermarket/show_spec.rb
+++ spec/features/supermarket/show_spec.rb
@@ -23,7 +23,6 @@ RSpec.describe 'the supermarket show page' do
 
   it "has a link to the supermarkets item index page" do
     supermarket_1 = Supermarket.create!(name: "Whole Foods", location: "Denver")
-    supermarket_2 = Supermarket.create!(name: "Shaws", location: "Millbury")
 
     customer_1 = Customer.create!(name: "Hellfire", supermarket_id: supermarket_1.id)
 
@@ -38,5 +37,25 @@ RSpec.describe 'the supermarket show page' do
     visit "/supermarkets/#{supermarket_1.id}"
 
     expect(page).to have_link("View all this supermarkets items")
+  end
+
+  it "the link takes you to all the supermarkets items" do
+    supermarket_1 = Supermarket.create!(name: "Whole Foods", location: "Denver")
+
+    customer_1 = Customer.create!(name: "Hellfire", supermarket_id: supermarket_1.id)
+
+    item_1 = Item.create!(name: "PB", price: 2)
+    item_2 = Item.create!(name: "Jelly", price: 4)
+    item_3 = Item.create!(name: "Bread", price: 6)
+
+    customer_item_1 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_1.id)
+    customer_item_2 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_2.id)
+    customer_item_3 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_3.id)
+
+    visit "/supermarkets/#{supermarket_1.id}"
+
+    click_on 'View all this supermarkets items'
+
+    expect(current_path).to eq("/supermarkets/#{supermarket_1.id}/items")
   end
 end
