c3e7cb39456965c543e142203fcb45e3e45414b5
Bryce Simonds <bsimonds14@gmail.com>
2022-07-31 14:34:44 -0600

    Test: has a uniq list of all items

--- spec/features/supermarket/show_spec.rb
+++ spec/features/supermarket/show_spec.rb
@@ -58,4 +58,27 @@ RSpec.describe 'the supermarket show page' do
 
     expect(current_path).to eq("/supermarkets/#{supermarket_1.id}/items")
   end
+
+  it "has a UNIQUE list of all the items that the supermarket has" do
+    supermarket_1 = Supermarket.create!(name: "Whole Foods", location: "Denver")
+
+    customer_1 = Customer.create!(name: "Hellfire", supermarket_id: supermarket_1.id)
+
+    item_1 = Item.create!(name: "PB", price: 2)
+    item_2 = Item.create!(name: "Jelly", price: 4)
+    item_3 = Item.create!(name: "Bread", price: 6)
+    item_4 = Item.create!(name: "Bread", price: 6)
+
+    customer_item_1 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_1.id)
+    customer_item_2 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_2.id)
+    customer_item_3 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_3.id)
+    customer_item_3 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_4.id)
+
+    visit "/supermarkets/#{supermarket_1.id}/items"
+
+    expect(page).to have_content("PB")
+    expect(page).to have_content("Jelly")
+    expect(page).to have_content(item_3.name)
+    expect(page).to_not have_content(item_4.name)
+  end
 end
