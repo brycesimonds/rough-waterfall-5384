c3de4f472beb99259e457773f8a638511cc47ee0
Bryce Simonds <bsimonds14@gmail.com>
2022-07-31 13:26:15 -0600

    Relationships all set up

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
-    has_many :customers
+  has_many :customer_items
+  has_many :customers, through: :customer_items
   end
--- spec/models/customer_item_spec.rb
+++ spec/models/customer_item_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe CustomerItems, type: :model do
+RSpec.describe CustomerItem, type: :model do
   describe 'relationships' do
     it { should belong_to :customer }
     it { should belong_to :item }
