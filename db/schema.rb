bcd409029d0159d41a85e773dd180bebad53425f
Bryce Simonds <bsimonds14@gmail.com>
2022-07-31 13:15:29 -0600

    Tests for relationships set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_31_190832) do
+ActiveRecord::Schema.define(version: 2022_07_31_191346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customer_items", force: :cascade do |t|
+    t.bigint "customer_id"
+    t.bigint "item_id"
+    t.index ["customer_id"], name: "index_customer_items_on_customer_id"
+    t.index ["item_id"], name: "index_customer_items_on_item_id"
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -37,5 +44,7 @@ ActiveRecord::Schema.define(version: 2022_07_31_190832) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "customer_items", "customers"
+  add_foreign_key "customer_items", "items"
   add_foreign_key "customers", "supermarkets"
 end
