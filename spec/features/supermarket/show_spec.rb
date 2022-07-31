ab3135ce19f7cb625ad1fa0f0fbd42e00676dcc0
Bryce Simonds <bsimonds14@gmail.com>
2022-07-31 15:21:58 -0600

    Test: 3 most popular items

--- spec/features/supermarket/show_spec.rb
+++ spec/features/supermarket/show_spec.rb
@@ -58,4 +58,43 @@ RSpec.describe 'the supermarket show page' do
 
     expect(current_path).to eq("/supermarkets/#{supermarket_1.id}/items")
   end
+
+  it "shows the three most popular items that are available in the supermarket" do
+    supermarket_1 = Supermarket.create!(name: "Whole Foods", location: "Denver")
+
+    customer_1 = Customer.create!(name: "Hellfire", supermarket_id: supermarket_1.id)
+    customer_2 = Customer.create!(name: "Coldocean", supermarket_id: supermarket_1.id)
+    customer_3 = Customer.create!(name: "Johnny", supermarket_id: supermarket_1.id)
+    customer_4 = Customer.create!(name: "Suzy", supermarket_id: supermarket_1.id)
+    customer_5 = Customer.create!(name: "Fiver", supermarket_id: supermarket_1.id)
+
+    item_1 = Item.create!(name: "PB", price: 2)
+    item_2 = Item.create!(name: "Jelly", price: 4)
+    item_3 = Item.create!(name: "Bread", price: 6)
+    item_4 = Item.create!(name: "Hot Sauce", price: 8)
+
+    customer_item_1 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_1.id)
+    customer_item_2 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_2.id)
+    customer_item_3 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_3.id)
+
+    customer_item_4 = CustomerItem.create!(customer_id: customer_2.id, item_id: item_1.id)
+    customer_item_5 = CustomerItem.create!(customer_id: customer_2.id, item_id: item_2.id)
+
+    customer_item_6 = CustomerItem.create!(customer_id: customer_3.id, item_id: item_4.id)
+
+    customer_item_7 = CustomerItem.create!(customer_id: customer_4.id, item_id: item_1.id)
+    customer_item_7 = CustomerItem.create!(customer_id: customer_4.id, item_id: item_3.id)
+
+    customer_item_8 = CustomerItem.create!(customer_id: customer_5.id, item_id: item_1.id)
+    customer_item_8 = CustomerItem.create!(customer_id: customer_5.id, item_id: item_2.id)
+
+    visit "/supermarkets/#{supermarket_1.id}"
+
+    within '#3-popular-items' do
+      expect(page).to have_content("PB")
+      expect(page).to have_content("Jelly")
+      expect(page).to have_content("Bread")
+      expect(page).to_not have_content("Hot Sauce")
+    end
+  end
 end
